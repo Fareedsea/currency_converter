3d7cf706ab45476ce8f5fc5865a61473090e14cd
Fareedsea <fareed.sea@gmail.com>
2024-04-21 20:09:35 +0500

    updates projects files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const currency = {
     EURO: 0.91,
     GBP: 0.76,
     INR: 74.57,
-    PKR: 280
+    PKR: 280,
 };
 let user_answer = await inquirer.prompt([
     {
@@ -25,20 +25,8 @@ let user_answer = await inquirer.prompt([
         type: 'number'
     }
 ]);
-if (user_answer.fcurrency == currency.USD) {
-    user_answer.fcurrency = 1;
-}
-else if (user_answer.fcurrency == currency.EURO) {
-    user_answer.fcurrency = 0.91;
-}
-else if (user_answer.fcurrency == currency.GBP) {
-    user_answer.fcurrency = 0.76;
-}
-else if (user_answer.fcurrency == currency.PKR) {
-    user_answer.fcurrency = 280;
-}
-let fromAmount = user_answer.fcurrency;
-let toAmount = user_answer.tcurrency;
+let fromAmount = currency[user_answer.fcurrency];
+let toAmount = currency[user_answer.tcurrency];
 let amount = user_answer.amount;
 let baseAmount = amount / fromAmount;
 let convertAmount = baseAmount * toAmount;
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import inquirer from "inquirer";
-const currency = {
+const currency: any = {
     USD: 1,
     EURO: 0.91,
     GBP: 0.76,
@@ -28,17 +28,9 @@ let user_answer = await inquirer.prompt([
     }
 ]
 );
-// if (user_answer.fcurrency == currency.USD){
-//     user_answer.fcurrency = 1;
-// } else if (user_answer.fcurrency == currency.EURO){
-//     user_answer.fcurrency = 0.91;
-// } else if (user_answer.fcurrency == currency.GBP){
-//     user_answer.fcurrency = 0.76;
-// } else if (user_answer.fcurrency == currency.PKR){
-//     user_answer.fcurrency = 280;
-// }
-let fromAmount = currency(user_answer.fcurrency);
-let toAmount = user_answer.tcurrency;
+
+let fromAmount = currency[user_answer.fcurrency];
+let toAmount = currency[user_answer.tcurrency];
 let amount = user_answer.amount;
 let baseAmount = amount / fromAmount;
 let convertAmount = baseAmount * toAmount;
